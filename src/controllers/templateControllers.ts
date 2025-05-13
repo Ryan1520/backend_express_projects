8efc6648242725705b900d787064288dbfa7d601
Tran Nguyen <trandainguyen1520@gmail.com>
2025-05-13 22:58:53 +0700

    add mongodb frame

--- src/controllers/templateControllers.ts
+++ src/controllers/templateControllers.ts
@@ -5,7 +5,25 @@ import { handleErrorWith } from "../utils/common";
 const controller1 = (req: Request, res: Response, next: NextFunction) => {
   handleErrorWith(next, "not_found", 404, { id: "123" });
 };
-const controller2 = (req: Request, res: Response, next: NextFunction) => {};
-const controller3 = (req: Request, res: Response, next: NextFunction) => {};
+const controller3 = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // await User.create({
+    //   name: "John Doe",
+    //   email: "abcd",
+    //   password: "123",
+    // });
+
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching user",
+      error: error,
+    });
+  }
+};
 
 export default { controller1, controller2, controller3 };
---
+++ src/model/User.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+const UserSchema: Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+  }
+);
+
+export default mongoose.model<IUser>('User', UserSchema);
